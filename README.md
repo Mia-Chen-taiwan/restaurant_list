a51f4b81aad9eeb9116365cbd7d36d2c8e505a05
陳昱伶 <mia89926@gmail.com>
2020-05-05 21:26:16 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# 我的餐廳清單網站
+
+使用node.js、express和express-handlebars所做的網站。
+
+## 網站功能
+
+1.在首頁看到所有餐廳與它們的簡單資料
+
+2.可點擊餐廳，看餐廳的詳細資訊
+
+3.可以透過搜尋餐廳名稱來找到特定的餐廳
+
+## 環境建置與需求
+
+node.js v10.15.0 -執行環境 express v4.14.7 -框架 express-handlebars v4.0.4 -模板引擎
+
+## 安裝與使用
+
+下載專案
+
+```bash
+git clone https://github.com/MiaChen/restaurant_list.git
+```
+
+安裝package
+
+```bash
+npm install
+```
+
+使用nodemon啟動伺服器
+
+```bash
+npm run dev
+```
+
+或正常啟動
+
+```bash
+npm start
+```
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:id', (req, res) => {
+    const restaurant = restaurantList.results.find(restaurant => req.params.id === restaurant.id.toString())
+    res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword
+    const restaurants = restaurantList.results.filter(restaurant => 
+       restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+      || restaurant.name_en.toLowerCase().includes(keyword.toLowerCase())
+      || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+    )
+    res.render('index', { restaurants: restaurants, keyword: keyword })
+  })
+
+app.listen(port, () => {
+    console.log(`Express is listening on localhost:${port}`)
+})
